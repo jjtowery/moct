d93f0c741015dda954303376de3af38064aca74b
jjtowery <debordle@gmail.com>
2025-07-22 08:50:35 -0600

    Challenging! Done with this for now.

--- Moct/MoctCat.lean
+++ Moct/MoctCat.lean
@@ -21,11 +21,11 @@ open CategoryTheory
 /-
   A moct induces a category in the usual sense.
   The objects are the image of dom, encoded as a subtype of α.
-  A morphism f : X → Y is an object with dom f = X and cdm f = Y.
+  A morphism f : X → Y is an object with dom f = X and cdm f = Y. 
   The identity morphism on X is the identity function on X. 
 -/
 
-def moct_cat {α : Type} {dom : α → α} {cdm : α → α} {R : α → α → α → Bool} [C : moct α dom cdm R] : 
+noncomputable def moct_cat {α : Type} {dom : α → α} {cdm : α → α} {R : α → α → α → Bool} [C : moct α dom cdm R] : 
     Category { a : α // ∃ x : α, dom x = a } where
   Hom X Y := { f : α // dom f = X.val ∧ cdm f = Y.val }
   id X := ⟨id X, ⟨by obtain ⟨x, h⟩ := X.property
@@ -34,9 +34,26 @@ def moct_cat {α : Type} {dom : α → α} {cdm : α → α} {R : α → α → 
                   by obtain ⟨x, h⟩ := X.property
                      rw [id, ←h]
                      exact (C.moct_exists_comp x (dom x) ⟨x, C.moct_right_id x⟩).symm⟩⟩
-  comp f g := have := C.moct_comp_exists g.val f.val (by rw [f.property.2, g.property.1])
-              sorry
-
-  /-⟨_, by obtain ⟨x, h⟩ := C.moct_comp_exists g.val f.val (by rw [f.property.2 , g.property.1])
-                     have := C.moct_dom_cdm_comp _ _ _ h
-                     _⟩-/
+  /- Here, I *want* to be able to do something like: 
+     comp f g := let ⟨h, hc⟩ := C.moct_comp_exists g.val f.val (by rw [f.property.2, g.property.1]) 
+                 ⟨h, ⟨by have := C.moct_dom_comp g.val f.val h hc
+                         rwa [f.property.1] at this,
+                      by have := C.moct_cdm_comp g.val f.val h hc
+                         rwa [g.property.2] at this⟩⟩
+    The issue is that this is elimiating an existential into a Type context, which is no good.
+    The solutions seem to be: change the semantics of moct, use choice, or maybe something more clever than I can come up with now.
+    For now, use choice just to do what I want to do.
+  -/
+  comp f g := let comp := C.moct_comp_exists g.val f.val (by rw [f.property.2, g.property.1])
+              let h := Classical.choose comp
+              let hc := Classical.choose_spec comp
+              ⟨h, ⟨by have := C.moct_dom_comp g.val f.val h hc
+                      rwa [f.property.1] at this, 
+                   by have := C.moct_cdm_comp g.val f.val h hc
+                      rwa [g.property.2] at this⟩⟩
+  id_comp := by intro X Y f
+                have c := C.moct_comp_exists f.val (dom f.val) (C.moct_exists_comp f.val (dom f.val) ⟨f.val, C.moct_right_id f.val⟩)
+                have := C.moct_map f.val (dom f.val) f.val (Classical.choose c) ⟨C.moct_right_id f.val, Classical.choose_spec c⟩
+                ext
+                simp
+                sorry
